9a1e93220de96bd1db28a89b42c053c547f01fd9
Ansesteri <angrylemon5678@gmail.com>
2024-02-01 20:10:11 +0200

    Add minor changes

--- account/views.py
+++ account/views.py
@@ -2,14 +2,27 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm, UserRegistrationForm
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 # Create your views here.
 
 @login_required
 def dashboard(request):
    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.files)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
 
 def register(request):
    if request.method == 'POST':
